ff2ec36034b2a2277ce816ab3026e981ea4b3f8c
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-11-19 11:35:35 -0300

    add test for get symbols

---
+++ src/test/java/me/calebeoliveira/broker/SymbolsControllerTest.java
@@ -0,0 +1,26 @@
+package me.calebeoliveira.broker;
+
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.json.tree.JsonNode;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@MicronautTest
+class SymbolsControllerTest {
+    @Inject
+    @Client("/symbols")
+    HttpClient client;
+
+    @Test
+    void shouldReturnListOfSymbols_whenCallingGetSymbolsEndpoint() {
+        var response = client.toBlocking().exchange("/", JsonNode.class);
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals(10, response.getBody().get().size());
+    }
+}
