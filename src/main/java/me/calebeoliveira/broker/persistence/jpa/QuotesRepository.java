4e823ebe7d39e9eb850db07438374afb6f8efe77
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2025-11-24 17:09:23 -0300

    add ordering example

--- src/main/java/me/calebeoliveira/broker/persistence/jpa/QuotesRepository.java
+++ src/main/java/me/calebeoliveira/broker/persistence/jpa/QuotesRepository.java
@@ -5,9 +5,14 @@ import io.micronaut.data.repository.CrudRepository;
 import me.calebeoliveira.broker.persistence.model.QuoteEntity;
 import me.calebeoliveira.broker.persistence.model.SymbolEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface QuotesRepository extends CrudRepository<QuoteEntity, Integer> {
     Optional<QuoteEntity> findBySymbol(SymbolEntity symbol);
+
+    // Ordering
+    List<QuoteEntity> listOrderByVolumeDesc();
+    List<QuoteEntity> listOrderByVolumeAsc();
 }
